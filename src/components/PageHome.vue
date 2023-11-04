e0cf56992582b67799c68cf7c2f0ae4665f10527
devxiaofan <devxiaofan@163.com>
2023-11-04 22:13:56 +0800

    show data

---
+++ src/components/PageHome.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-home">
+    <div v-for="thread in threads" :key="thread.id">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{users.find(u => u.id === posts.find(p => p.id === postId).userId).name}}</p>
+        <p>{{ posts.find(p => p.id === postId).text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SourceData from '@/data.json'
+
+export default {
+  name: 'PageHome',
+  data () {
+    return {
+      threads: SourceData.threads,
+      posts: SourceData.posts,
+      users: SourceData.users
+    }
+  }
+}
+</script>
+
+<style scoped></style>
