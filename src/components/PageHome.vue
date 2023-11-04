489102eae17ec9330e31917823dbd9abb727bc92
devxiaofan <devxiaofan@163.com>
2023-11-04 23:02:05 +0800

    show post css1

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,30 +1,52 @@
 <template>
-  <div class="page-home">
-    <div v-for="thread in threads" :key="thread.id">
-      <h2>{{ thread.title }}</h2>
-      <div v-for="postId in thread.posts" :key="postId">
-        <p>{{userById(postById(postId).userId).name}}</p>
-        <p>{{ postById(postId).text }}</p>
+  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+    <h1>{{thread.title}}</h1>
+
+    <div class="posts-list">
+      <div class="post" v-for="postId in thread.posts" :key="postId">
+        <div class="user-info">
+          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
+          <a href="#">
+            <img :src="userById(postById(postId).userId).avatar" alt="avatar" class="avatar-large">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>{{postById(postId).text}}</p>
+          </div>
+        </div>
+
+        <div  class="post-date">
+          <p class="text-small">{{postById(postId).publishedAt}}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script>
 import sourceData from '@/data.json'
-import { ref } from 'vue'
+console.log(sourceData)
 
-const threads = ref(sourceData.threads)
-const posts = ref(sourceData.posts)
-const users = ref(sourceData.users)
-
-function postById (id) {
-  return posts.value.find(p => p.id === id)
-}
-
-function userById (id) {
-  return users.value.find(u => u.id === id)
+export default {
+  name: 'PageHome',
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (id) {
+      return this.posts.find(p => p.id === id)
+    },
+    userById (id) {
+      return this.users.find(u => u.id === id)
+    }
+  }
 }
 </script>
-
-<style scoped></style>
