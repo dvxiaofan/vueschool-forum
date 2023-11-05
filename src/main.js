8b2eaa67a6242e80550e6ede9054f87a70a10bc2
devxiaofan <devxiaofan@163.com>
2023-11-05 11:12:24 +0800

    vue-router1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PageHome from '@/components/PageHome.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { path: '/', component: PageHome, name: 'Home' }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 const forumApp = createApp(App)
 forumApp.component('NiceButton', {})
-// forumApp.use(router)
+forumApp.use(router)
 forumApp.mount('#app')
 
 const forumApp2 = createApp(App)
