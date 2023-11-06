3b5195b820852dd708eeca0d20fa0923bc33b097
devxiaofan <devxiaofan@163.com>
2023-11-06 09:20:33 +0800

    add not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
+import PageNotFound from '@/components/PageNotFound.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   {
@@ -12,7 +14,29 @@ const routes = [
     path: '/thread/:id',
     component: PageThreadShow,
     name: 'ThreadShow',
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      // if exists continue
+      if (threadExists) {
+        return next()
+      } else {
+        // else redirect to not found
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: PageNotFound,
+    name: 'NotFound'
   }
 ]
 
