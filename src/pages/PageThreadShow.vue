79cd904cf2c36b16332a6a5a403bbda86c030359
devxiaofan <1353815633@qq.com>
2023-12-18 16:41:37 +0800

    extract posts to a component

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,35 +1,18 @@
 <template>
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
-    <div class="posts-list">
-      <div class="post" v-for="postId in thread.posts" :key="postId">
-        <div class="user-info">
-          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
-          <a href="#">
-            <img :src="userById(postById(postId).userId).avatar" alt="avatar" class="avatar-large">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>{{postById(postId).text}}</p>
-          </div>
-        </div>
-
-        <div  class="post-date">
-          <p class="text-small">{{postById(postId).publishedAt}}</p>
-        </div>
-      </div>
-    </div>
+    <post-list :posts="threadPosts"></post-list>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import PostList from '@/components/PostList.vue'
 export default {
   name: 'PageThreadShow',
+  components: {
+    PostList
+  },
   props: {
     id: {
       required: true,
@@ -39,21 +22,15 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
+      posts: sourceData.posts
     }
   },
   computed: {
     thread () {
       return this.threads.find(thread => thread.id === this.id)
-    }
-  },
-  methods: {
-    postById (id) {
-      return this.posts.find(p => p.id === id)
     },
-    userById (id) {
-      return this.users.find(u => u.id === id)
+    threadPosts () {
+      return this.posts.filter(post => post.threadId === this.id)
     }
   }
 }
